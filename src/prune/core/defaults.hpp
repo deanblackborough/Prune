807c171f4637e95207adb26f23073436381d6e9a
Dean Blackborough <dean@g3d-development.com>
2026-05-02 18:10:13 +0100

    Fix includes

--- src/prune/core/defaults.hpp
+++ src/prune/core/defaults.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string_view>
+
 namespace prune {
 
     constexpr int k_default_object_size = 16;
--- src/prune/tooling/inspector.cpp
+++ src/prune/tooling/inspector.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <string_view>
 
 #include "imgui.h"
 
