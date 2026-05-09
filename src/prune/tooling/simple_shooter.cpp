a0092d99564927d06d6ed25c2e62ba130b5a3f2d
Dean Blackborough <dean@g3d-development.com>
2026-05-09 23:16:05 +0100

    Fixed spelling error

--- src/prune/tooling/simple_shooter.cpp
+++ src/prune/tooling/simple_shooter.cpp
@@ -7,7 +7,7 @@ namespace prune {
 	{
         if (tooling::imgui::layout::collapsing_header("Game")) {
             if (tooling::imgui::property_table::begin("##game")) {
-                tooling::imgui::property_table::slider_float("Enemey Speed", "enemey_speed", options.enemy_speed, 0.0f, 160.0f);
+                tooling::imgui::property_table::slider_float("Enemy Speed", "enemy_speed", options.enemy_speed, 0.0f, 160.0f);
                 tooling::imgui::property_table::slider_float("Bullet Speed", "bullet_speed", options.bullet_speed, 40.0f, 400.0f);
                 tooling::imgui::property_table::slider_float("Bullet Lifetime", "bullet_lifetime", options.bullet_lifetime, 0.2f, 4.0f);
                 tooling::imgui::property_table::end();
