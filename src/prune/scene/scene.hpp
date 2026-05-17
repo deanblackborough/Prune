0455a28119fb30deea62d7515205727a1507ee3e
Dean Blackborough <dean@g3d-development.com>
2026-05-17 15:18:13 +0100

    Generic scene class

---
+++ src/prune/scene/scene.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string_view>
+
+#include <SDL2/SDL.h>
+
+#include "prune/core/input.hpp"
+#include "prune/scene/game_object_manager.hpp"
+#include "prune/scene/player_controller.hpp"
+#include "prune/scene/scene_camera.hpp"
+#include "prune/scene/scene_state.hpp"
+
+namespace prune {
+
+    class Scene {
+    public:
+        virtual ~Scene() = default;
+
+        Scene(const Scene&) = delete;
+        Scene& operator=(const Scene&) = delete;
+
+        virtual void on_enter() = 0;
+        virtual void on_exit() = 0;
+        virtual void update(float dt, const Input& input) = 0;
+        virtual void render(SDL_Renderer* renderer) = 0;
+
+        virtual void new_scene() = 0;
+
+        [[nodiscard]] virtual bool save_to_file(std::string_view path, std::string& error) const = 0;
+        [[nodiscard]] virtual bool load_from_file(std::string_view path, std::string& error) = 0;
+
+        virtual void set_viewport(const SceneViewport& viewport) noexcept = 0;
+        [[nodiscard]] virtual const SceneViewport& get_viewport() const noexcept = 0;
+        [[nodiscard]] virtual int get_viewport_width() const noexcept = 0;
+        [[nodiscard]] virtual int get_viewport_height() const noexcept = 0;
+
+        [[nodiscard]] virtual std::string_view scene_name() const noexcept = 0;
+        [[nodiscard]] virtual std::string_view scene_tools_label() const noexcept = 0;
+        virtual void draw_scene_tools(bool& open) = 0;
+
+        virtual GameObjectManager& get_object_manager() = 0;
+        [[nodiscard]] virtual GameObjectId get_player_id() const = 0;
+        virtual PlayerController& get_player_controller() = 0;
+        virtual GridOptions& get_grid_options() = 0;
+        virtual SceneOptions& get_scene_options() = 0;
+
+        virtual SceneCamera& get_camera() noexcept = 0;
+        [[nodiscard]] virtual const SceneCamera& get_camera() const noexcept = 0;
+
+    protected:
+        Scene() = default;
+    };
+}
---
+++ src/prune/scene/simple_shooter_serializer.cpp
@@ -0,0 +1,41 @@
+#include "prune/scene/simple_shooter_serializer.hpp"
+
+namespace prune {
+
+    void SimpleShooterSerializer::save_to_node(const SimpleShooterState& state, YAML::Node& root)
+    {
+        root["simple_shooter"]["paused"] = state.options.paused;
+        root["simple_shooter"]["enemy_speed"] = state.options.enemy_speed;
+        root["simple_shooter"]["bullet_speed"] = state.options.bullet_speed;
+        root["simple_shooter"]["bullet_lifetime"] = state.options.bullet_lifetime;
+    }
+
+    bool SimpleShooterSerializer::load_from_node(
+        const YAML::Node& root,
+        SimpleShooterState& state,
+        std::string& error
+    )
+    {
+        const YAML::Node simple_shooter = root["simple_shooter"];
+
+        if (!simple_shooter || !simple_shooter.IsMap()) {
+            error = "Save file is missing simple_shooter section.";
+            return false;
+        }
+
+        if (!simple_shooter["paused"] ||
+            !simple_shooter["enemy_speed"] ||
+            !simple_shooter["bullet_speed"] ||
+            !simple_shooter["bullet_lifetime"]) {
+            error = "simple_shooter options are incomplete.";
+            return false;
+        }
+
+        state.options.paused = simple_shooter["paused"].as<bool>();
+        state.options.enemy_speed = simple_shooter["enemy_speed"].as<float>();
+        state.options.bullet_speed = simple_shooter["bullet_speed"].as<float>();
+        state.options.bullet_lifetime = simple_shooter["bullet_lifetime"].as<float>();
+
+        return true;
+    }
+}
