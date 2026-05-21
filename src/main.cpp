bd8bedf035596d3bfda8f90df93bc84477a787da
Dean Blackborough <dean@g3d-development.com>
2026-05-21 13:19:20 +0100

    Don't override defaults in main

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,6 @@ int main()
     try {
         prune::AppConfig config;
         config.window.title = "Prune";
-        config.window.width = 1280;
-        config.window.height = 720;
-        config.fixed_timestep = 1.0f / 60.0f;
 
         prune::App app(config);
         app.run();
