887ad1f81462c3c74611d7b8b25a5b833c57ce72
Dean Blackborough <dean@g3d-development.com>
2026-05-25 15:54:32 +0100

    Remove simple shooter include from stats

--- src/prune/tooling/stats.hpp
+++ src/prune/tooling/stats.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "prune/scene/game_object_manager.hpp"
-#include "prune/scene/simple_shooter/simple_shooter_scene.hpp"
+#include "prune/scene/scene_state.hpp"
+#include "prune/scene/scene_camera.hpp"
 
 namespace prune {
 
